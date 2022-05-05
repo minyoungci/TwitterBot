a8fe5ca8b77828a317ca269d66cd755aba92f3cd
minyoungci <dbssus123@gmail.com>
2022-05-06 00:47:44 +0900

    first

---
+++ TwitterBot.py
@@ -0,0 +1,7 @@
+import tweepy
+
+api_key = "oRJ4KE0NAwPyHDoy4xP59viYH"
+api_secret_key = "mVw8NYrp1dodavtZN3ym537nrYmkGDiH4ylXwjaRWosC6pFGv4"
+
+auth = tweepy.OAuthHandler("api_key", "api_secret_key")
+auth.set_access_token("")
