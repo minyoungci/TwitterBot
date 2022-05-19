e6f0a34ca027749a1fd086a6a75f62acfff82a2c
minyoungci <dbssus123@gmail.com>
2022-05-19 13:44:18 +0900

    users image url 찾기

--- main.py
+++ main.py
@@ -3,16 +3,19 @@ import config
 
 client = tweepy.Client(bearer_token=config.bearer_token)
 
-query = "korea -is:retweet"
+query = "covid -is:retweet"
 
 response = client.search_recent_tweets(
     query=query,
     max_results=100,
-    tweet_fields=["created_at", "lang"],
+    user_fields=["profile_image_url"],
     expansions=["author_id"],
 )
 
-for tweet in response.data:
-    print(tweet.id)
-    print(tweet.lang)
+users = {u["id"]: u for u in response.includes["users"]}
 
+for tweet in response.data:
+    if users[tweet.author_id]:
+        user = users[tweet.author_id]
+        print(tweet.id)
+        print(user.profile_image_url)
